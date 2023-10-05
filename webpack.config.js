6833c1b153b37109b2389bb24af8c8c8d518aeca
Kehinde Oloruntayo <kehindeoloruntayo1@gmail.com>
2023-10-05 12:22:17 +0100

    add webpack.config.js file

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const Dotenv = require('dotenv-webpack')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    static: './dist'
+  },
+  plugins: [
+    new Dotenv(),
+    // new webpack.DefinePlugin({
+    //   'process.env': JSON.stringify(process.env),
+    // }),
+    new CleanWebpackPlugin(),
+    new ESLintPlugin({
+      context: "compiler.context",
+      eslintPath: "eslint",
+      extensions: 'js',
+      exclude: 'node_modules',
+      fix: false,
+      formatter: 'stylish',
+      lintDirtyModulesOnly: false,
+      threads: false,
+      emitError: true,
+      emitWarning: true,
+      failOnError: true,
+      failOnWarning: false,
+      quiet: false,
+      outputReport: false
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Shape Tracker',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      }
+    ]
+  }
+};
